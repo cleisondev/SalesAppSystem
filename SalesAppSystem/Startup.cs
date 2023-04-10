2e56bd125a4992662955fdc8b2f9193028064eb1
Cleison Nunes <cleison.nunes20@gmail.com>
2023-04-10 19:46:03 -0300

    First Migration

--- SalesAppSystem/Startup.cs
+++ SalesAppSystem/Startup.cs
@@ -37,7 +37,8 @@ namespace SalesAppSystem
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<SalesAppSystemContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("SalesAppSystemContext")));
+                    options.UseMySql(Configuration.GetConnectionString("SalesAppSystemContext"), builder =>
+                    builder.MigrationsAssembly("SalesAppSystem")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
