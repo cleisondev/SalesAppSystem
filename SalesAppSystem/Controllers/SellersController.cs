6d50bd30d662f77eb3fb1251746915a07792c460
Cleison Nunes <cleison.nunes20@gmail.com>
2023-04-14 08:02:18 -0300

    Customizing Error page

--- SalesAppSystem/Controllers/SellersController.cs
+++ SalesAppSystem/Controllers/SellersController.cs
@@ -5,6 +5,7 @@ using SalesAppSystem.Services;
 using SalesAppSystem.Services.Exceptions;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -44,11 +45,11 @@ namespace SalesAppSystem.Controllers
         {
             if(id == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = "Id not provided" });
             }
 
             var obj = _sellerService.FindById(id.Value);
-            if(obj == null) { return NotFound(); }
+            if(obj == null) { return RedirectToAction(nameof(Error), new { message = "Id not found" }); }
             return View(obj);
         }
 
@@ -65,11 +66,11 @@ namespace SalesAppSystem.Controllers
 
             if (id == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = "Id not provided" });
             }
 
             var obj = _sellerService.FindById(id.Value);
-            if (obj == null) { return NotFound(); }
+            if (obj == null) { return RedirectToAction(nameof(Error), new { message = "Id not found" }); }
             return View(obj);
         }
 
@@ -78,13 +79,13 @@ namespace SalesAppSystem.Controllers
 
             if (id == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = "Id not provided" });
             }
 
             var obj = _sellerService.FindById(id.Value);
             if(obj == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = "Id not found" });
             }
 
             List<Department> departments = _departmentService.FindAll();
@@ -99,7 +100,7 @@ namespace SalesAppSystem.Controllers
         {
             if(id != seller.Id)
             {
-                return BadRequest();
+                return RedirectToAction(nameof(Error), new { message = "Id incompatible" });
             }
 
             try
@@ -108,15 +109,20 @@ namespace SalesAppSystem.Controllers
                 return RedirectToAction(nameof(Index));
             }
 
-            catch(NotFoundException)
+            catch(ApplicationException e)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = e.Message });
             }
-            catch (DbConcurrencyException)
+           }
+
+        public IActionResult Error(string message)
+        {
+            var viewModel = new ErrorViewModel
             {
-                return BadRequest();
-            }
-           
+                Message = message,
+                RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
+            };
+            return View(viewModel);
         }
     }
 }
